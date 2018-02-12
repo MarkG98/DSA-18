51735cb36ffd00c8f9c822c64487bc1ce65d3bc3
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-02-12 17:49:31 -0500

    Completed Sorting Day 4

--- sorting/day00/src/Boomerang.java
+++ sorting/day00/src/Boomerang.java
@@ -1,8 +1,43 @@
+import java.util.HashMap;
+import java.util.Collection;
+
 public class Boomerang {
 
     public static int numberOfBoomerangs(int[][] points) {
-        // TODO
-        return 0;
+        HashMap<Double,Integer> hm = new HashMap<>();
+        int count;
+        int total = 0;
+
+
+        for (int j = 0; j < points.length; j++)
+        {
+            for (int i = 0; i < points.length; i++)
+            {
+                if (i != j)
+                {
+                    double dist = Math.sqrt((points[j][0]-points[i][0])*(points[j][0] - points[i][0]) + (points[j][1] - points[i][1])*(points[j][1]-points[i][1]));
+                    if (hm.containsKey(dist))
+                    {
+                        count = hm.get(dist);
+                        hm.put(dist,count + 1);
+                    }
+                    else
+                    {
+                        hm.put(dist,1);
+                    }
+                }
+
+            }
+            Collection <Integer> list = hm.values();
+            for (int val: list)
+            {
+                total += val * (val - 1);
+            }
+            hm.clear();
+        }
+
+        return total;
+
     }
 }
 
--- sorting/day00/src/MergeSort.java
+++ sorting/day00/src/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class MergeSort extends SortAlgorithm {
 
@@ -10,14 +11,29 @@ public class MergeSort extends SortAlgorithm {
      * TODO
      * Best-case runtime:
      * Worst-case runtime:
-     * Average-case runtime:
+     * Average-case runtime: O(nlogn)
      *
      * Space-complexity:
      */
     @Override
     public int[] sort(int[] array) {
-        // TODO
-        return new int[0];
+        if (array.length == 1)
+        {
+            return array;
+        }
+        else if (array.length <= 10)
+        {
+            InsertionSort IS = new InsertionSort();
+            return IS.sort(array);
+        }
+
+        int[] left = Arrays.copyOfRange(array,0,array.length/2);
+        int[] right = Arrays.copyOfRange(array,array.length/2, array.length);
+
+        left = sort(left);
+        right = sort(right);
+
+        return merge(left,right);
     }
 
     /**
@@ -26,8 +42,42 @@ public class MergeSort extends SortAlgorithm {
      * Use Insertion Sort if the length of the array is <= INSERTION_THRESHOLD
      */
     public int[] merge(int[] a, int[] b) {
-        // TODO
-        return new int[0];
+
+        int[] s = new int[a.length + b.length];
+
+        int x = 0;
+        int y = 0;
+        int l = 0;
+
+        while (x < a.length && y < b.length)
+        {
+            if(a[x] <= b[y])
+            {
+                s[l] = a[x];
+                x++;
+                l++;
+            }
+            else
+            {
+                s[l] = b[y];
+                y++;
+                l++;
+            }
+        }
+
+        while (x < a.length)
+        {
+            s[l] = a[x];
+            x++;
+            l++;
+        }
+        while(y < b.length)
+        {
+            s[l] = b[y];
+            y++;
+            l++;
+        }
+        return s;
     }
 
 }
