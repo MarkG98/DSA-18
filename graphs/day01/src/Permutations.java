f8e74f6dd6777e6507a3bcba92084032d3d045a9
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-03-29 16:41:02 -0400

    Done with permutations

--- graphs/day01/src/Permutations.java
+++ graphs/day01/src/Permutations.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -18,7 +19,14 @@ public class Permutations {
 
     public static List<List<Integer>> permutations(List<Integer> A) {
         // TODO
+        Set<Integer> unused = new HashSet<>();
+        for (int u : A)
+        {
+            unused.add(u);
+        }
+        LinkedList<Integer> curr = new LinkedList<>();
         List<List<Integer>> permutations = new LinkedList<>();
+        backtrack(curr,unused,permutations);
         return permutations;
     }
 
