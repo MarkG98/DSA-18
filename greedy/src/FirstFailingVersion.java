211a825883fdcf25b8b1d5d2c4c4a3364c89e323
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-04-19 20:56:21 -0400

    Finished greedy

--- greedy/src/FirstFailingVersion.java
+++ greedy/src/FirstFailingVersion.java
@@ -1,7 +1,13 @@
 public class FirstFailingVersion {
 
     public static long firstBadVersion(long n, IsFailingVersion isBadVersion) {
-        // TODO
-        return -1;
+            for (long i = 0; i <= n; i++) {
+                if (!isBadVersion.isFailingVersion(i) && isBadVersion.isFailingVersion(i + 1))
+                    return (i+1);
+                if (isBadVersion.isFailingVersion(n-i) && !isBadVersion.isFailingVersion(n - (i + 1)))
+                    return n - i;
+            }
+            return n;
     }
 }
+
--- greedy/src/MCCR.java
+++ greedy/src/MCCR.java
@@ -1,7 +1,56 @@
 public class MCCR {
         public static int MCCR(EdgeWeightedGraph G) {
             // TODO
-            return 0;
+            Edge[] path = new Edge[G.numberOfV()];
+            int[] distTo = new int[G.numberOfV()];
+            boolean[] closed = new boolean[G.numberOfV()];
+            IndexPQ<Integer> Q = new IndexPQ<>(G.numberOfE());
+
+            for (int i = 0; i < distTo.length; i++) {
+                distTo[i] = Integer.MAX_VALUE;
+            }
+
+            for (int v = 0; v < G.numberOfV(); v++) {
+                if (!closed[v])
+                {
+                    distTo[v] = 0;
+                    Q.insert(v,distTo[v]);
+                    while (!Q.isEmpty())
+                    {
+                        int min = Q.delMin();
+                        closed[min] = true;
+
+                        for (Edge edge : G.edges(min))
+                        {
+                            int o = edge.other(min);
+                            if (edge.weight() < distTo[o] && !closed[o])
+                            {
+                                distTo[o] = edge.weight();
+                                path[o] = edge;
+                                if (!Q.contains(o))
+                                {
+                                    Q.insert(o,distTo[o]);
+                                }
+                                else
+                                {
+                                    Q.decreaseKey(o,distTo[o]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int cost = 0;
+
+            for (Edge edge : path)
+            {
+                if (edge != null)
+                {
+                    cost += edge.weight();
+                }
+            }
+            return cost;
         }
 
     }
