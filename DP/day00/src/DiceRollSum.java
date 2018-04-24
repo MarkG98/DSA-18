ddc1d09a5212e1a04c646508e7742f65e173d430
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-04-23 20:16:18 -0400

    Finished DP Day 0

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,40 @@
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(6n) --> O(n)
+    // Space: O(n)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        int[] DP = new int[N + 1];
+        for (int i = 0; i < DP.length; i++) {
+            DP[i] = -1;
+        }
+        return KMEANS(N, DP);
     }
+
+    private static int KMEANS(int N, int[] DP) {
+        // base cases
+        if (N == 1 || N == 0) {
+            return 1;
+        }
+        if (N < 0) {
+            return 0;
+        }
+
+        // check if already seen
+        if (DP[N] != -1) {
+            return DP[N];
+        }
+
+        // try all guesses
+        int total = 0;
+        for (int i = 1; i <= 6; i++) {
+            total += KMEANS(N - i, DP);
+        }
+
+        // store and return result
+        DP[N] = total;
+        return total;
+    }
+
+
 
 }
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,25 @@
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(n^2)
+    // Space: O(n)
     public static int LIS(int[] A) {
         // TODO
-        return 0;
+        if (A.length == 0) {
+            return 0;
+        }
+        int[] DP = new int[A.length];
+        DP[0] = 1;
+        int answer = 1;
+        for (int i = 1; i < DP.length; i++) {
+            int maxVal = 0;
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j]) {
+                    maxVal = Math.max(maxVal, DP[j]);
+                }
+            }
+            DP[i] = maxVal + 1;
+            answer = Math.max(answer, DP[i]);
+        }
+        return answer;
     }
 }
